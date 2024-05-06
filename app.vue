f51103b5d09392418b7b3d5e34e73c96c1e090ab
zhangbafa <youdan2020@qq.com>
2024-05-06 22:27:06 +0800

    add autodl.com

--- app.vue
+++ app.vue
@@ -73,6 +73,15 @@ const llm = [
     ]
   },
   {
+    "算力":[
+      {
+        name:'AutoDL',
+        url:'https://www.autodl.com/',
+        image:'autodl.png'
+      }
+    ]
+  },
+  {
     "LLM（全球）": [
       {
         name: "ChatGPT",
@@ -567,13 +576,13 @@ useSeoMeta({
 
 <template>
   <div class="relative">
-    <div class="text-center text-4xl font-bold m-12 mt-14">AI精选合集</div>
+    <div class="text-center text-4xl font-bold m-12 mt-14">AI 精选合集</div>
     <div v-for="(obj, index) in llm" :key="index" class="w-11/12 md:w-8/12 mx-auto mb-10 mt-16">
       <div v-for="(value, key) in obj" :key="key" >
-        <h2  class="mt-4 mb-6 font-bold">{{ key }}</h2>
+        <h1  class="mt-4 mb-6 font-bold text-gray-600">{{ key }}</h1>
         <div class="grid grid-cols-2 mg:grid-cols-2 lg:grid-cols-4 gap-6">
           <div v-for="(item, i) in value" :key="i" class="text-center">
-          <div class="border rounded-md shadow-sm hover:bg-gray-100 transition duration-300">
+          <div class="border rounded-md shadow-sm hover:bg-gray-50 transition duration-300">
             <a :href="item.url" target="_blank" class="block w-full h-full p-4">
               <img :src="'/downloads/' + item.image" :alt="item.name" style="width: 50px;min-height:50px;border-radius: 10px;margin:0 auto"/>
               <p class="mt-4 text-gray-500 dark:text-gray-500">{{ item.name }}</p>
